34d5d6e72570fed7b225e6f4156b8feb5f47b13a
7oda <7oda@gmail.com>
2024-12-05 22:02:25 +0200

    + bottomNavBar

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+abstract class AppColors {
+  static const scaffoldBg = Color(0xFF121312);
+  static const bottomNavBar = Color(0xFF1A1A1A);
+  static const selectedBottomNavBarItem = Color(0xFFFFBB3B);
+  static const unSelectedBottomNavBarItem = Color(0xFFC6C6C6);
+}
---
+++ lib/my_app/my_app.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:movies_app/config/theme/app_theme.dart';
+import 'package:movies_app/routing/router.dart';
+import 'package:movies_app/routing/routes.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(412, 892),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: AppTheme.theme,
+          onGenerateRoute: AppRouter.router,
+          initialRoute: AppRoutes.home,
+        );
+      },
+    );
+  }
+}
